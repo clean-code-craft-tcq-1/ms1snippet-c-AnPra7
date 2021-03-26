8388bcfa6a4eebe8594ab838131e5a80f0f7cfd2
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-03-26 08:24:30 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -6,29 +6,29 @@
 TEST_CASE("reports error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(validateSensorreadings(socReadings, numOfSocReadings,0.05 ) == 0);
+  REQUIRE(validateSensorreadings(socReadings, numOfSocReadings,SOCthreshold) == 0);
 }
 
 TEST_CASE("reports error when current jumps abruptly") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(validateSensorreadings(currentReadings, numOfCurReadings,0.1 ) == 1);
+  REQUIRE(validateSensorreadings(currentReadings, numOfCurReadings,Currentthreshold) == 1);
 }
 TEST_CASE("Tests error when current jumps abruptly") {
   double currentReadings[] = {1.0, 2, 3, 4};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(validateSensorreadings(currentReadings, numOfCurReadings,0.1 ) == 0);
+  REQUIRE(validateSensorreadings(currentReadings, numOfCurReadings,Currentthreshold) == 0);
 }
 
 TEST_CASE("reports no error when current jumps abruptly") {
   double currentReadings[] = {0.0, 0.0, 0.0, 0.0};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(validateSensorreadings(currentReadings, numOfCurReadings,0.1) == 1);
+  REQUIRE(validateSensorreadings(currentReadings, numOfCurReadings,Currentthreshold) == 1);
 }
 
 TEST_CASE("reports no error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.0, 0.0, 0.0};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(validateSensorreadings(socReadings, numOfSocReadings,0.05 ) == 1);
+  REQUIRE(validateSensorreadings(socReadings, numOfSocReadings,SOCthreshold) == 1);
 }
 
