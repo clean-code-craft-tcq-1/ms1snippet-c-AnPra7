89edbb6efecae02fb2515a629cf4e0031c41afa7
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-03-26 08:44:43 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -2,6 +2,7 @@
 
 #include "test/catch.hpp"
 #include "sensor-validate.h"
+#include <stddef.h>
 
 TEST_CASE("reports error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
