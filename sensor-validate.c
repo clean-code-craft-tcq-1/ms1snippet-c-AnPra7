a2563ebd21e06f3e2f37c3caf9d4c7c80b672372
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-03-26 21:02:53 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,14 +1,31 @@
 #include "sensor-validate.h"
 
 
-
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    IsThereSuddenJump
+ */
+/*!    \brief      to check if there is a sudden junmp
+ * 
+ *     \param       current value ,next value and max value
+ *     \returns     the status if the jump was abrupt
+ *
+*//*------------------------------------------------------------------------*/
 int IsThereSuddenJump(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return 0;
   }
   return 1;
 }
-
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    validateSensorreadings
+ */
+/*!    \brief       to check if the sensor readings are null or have values and
+ *                  send for check for jump
+ * 
+ *     \param       sensor values ,number of values  and and threshold of jump
+ *     \returns     the status if the jump was abrupt
+ *
+*//*------------------------------------------------------------------------*/
 int validateSensorreadings(double* values, int numOfValues,double thresholdofJump)
 {
   
@@ -23,7 +40,15 @@ int validateSensorreadings(double* values, int numOfValues,double thresholdofJum
   return IsValidJump;
 }
   
-
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    IfNotNullCheckForJump
+ */
+/*!    \brief       sending value to check jump
+ * 
+ *     \param        sensor values ,number of values  and and threshold of jump
+ *     \returns    the status if the jump was abrupt
+ *
+*//*------------------------------------------------------------------------*/
 int IfNotNullCheckForJump(double* values, int numOfValues,double thresholdofJump) {
   
   int IsValidTransition = 1;
